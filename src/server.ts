7cdb2920fa14bbb6fae24a144b158aa68e9d1d73
Israel Teixeira <israeltx@gmail.com>
2025-07-04 14:20:57 -0300

    Create GET request

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express"
 import { knex } from "./database/knex"
+import { request } from "http"
 
 const app = express()
 app.use(express.json())
@@ -7,10 +8,16 @@ app.use(express.json())
 app.post("/courses", async (request: Request, response: Response) => {
   const {name} = request.body
 
-  // await knex('courses').insert({name})
-  await knex.raw('INSERT INTO courses (name) VALUES (?)', [name])
+  await knex('courses').insert({name})
+  // await knex.raw('INSERT INTO courses (name) VALUES (?)', [name])
 
   response.status(201).json()
+})
+
+app.get("/courses", async (request: Request, response:Response) => {
+  // const courses = await knex.raw('SELECT * FROM courses')
+  const courses = await knex('courses').select()
+  response.json(courses)
 })
 
 app.listen(3333, () => console.log(`Server is running on port 3333`))

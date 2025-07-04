dda015123f27aa6ccee14af11719598ed7c993ca
Israel Teixeira <israeltx@gmail.com>
2025-07-04 14:25:28 -0300

    Create a PUT request for updates

--- src/server.ts
+++ src/server.ts
@@ -11,13 +11,22 @@ app.post("/courses", async (request: Request, response: Response) => {
   await knex('courses').insert({name})
   // await knex.raw('INSERT INTO courses (name) VALUES (?)', [name])
 
-  response.status(201).json()
+  return response.status(201).json()
 })
 
 app.get("/courses", async (request: Request, response:Response) => {
   // const courses = await knex.raw('SELECT * FROM courses')
   const courses = await knex('courses').select()
-  response.json(courses)
+  return response.json(courses)
+})
+
+app.put("/courses/:id", async (request: Request, response:Response) => {
+  const {id} = request.params
+  const {name} = request.body
+
+  await knex('courses').update({name}).where({id})
+  
+  return response.json()
 })
 
 app.listen(3333, () => console.log(`Server is running on port 3333`))
